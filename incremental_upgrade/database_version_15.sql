bc1a980a37dc0489d5ba47615537eb79de31644d
Mark Whalley <mark@ipx.co.uk>
2022-09-28 23:07:59 +0100

    v15: currency_type for crypto upgrade

--- incremental_upgrade/database_version_15.sql
+++ incremental_upgrade/database_version_15.sql
@@ -3,7 +3,29 @@
 -- so only upgrade from v1-v7 possible. Need to downgrade other versions prior to upgrade 
 alter table CURRENCYFORMATS_V1 add column CURRENCY_TYPE TEXT;
 update CURRENCYFORMATS_V1 set CURRENCY_TYPE = 'Fiat';
-update CURRENCYFORMATS_V1 set CURRENCY_TYPE = 'Crypto' where CURRENCY_SYMBOL = 'BTC';
+update CURRENCYFORMATS_V1 set CURRENCY_TYPE = 'Crypto' where CURRENCY_SYMBOL NOT IN
+('ADP', 'AED', 'AFA', 'AFN', 'ALK', 'ALL', 'AMD', 'ANG', 'AOA', 'AOK', 'AON', 'AOR', 'ARA', 
+'ARP', 'ARS', 'ARY', 'ATS', 'AUD', 'AWG', 'AYM', 'AZM', 'AZN', 'BAD', ' BAM', 'BBD', 'BDT', 
+'BEC', 'BEF', 'BEL', 'BGJ', 'BGK', 'BGL', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BOP', 
+'BRB', 'BRC', 'BRE', 'BRL', 'BRN', 'BRR', 'BSD', ' BTN', 'BUK', 'BWP', 'BYB', 'BYN', 'BYR', 
+'BZD', 'CAD', 'CDF', 'CHC', 'CHF', 'CLP', 'CNY', 'COP', 'CRC', 'CSD', 'CSJ', 'CSK', 'CUC', 
+'CUP', 'CVE', 'CYP', 'CZK', ' DDM', 'DEM', 'DJF', 'DKK', 'DOP', 'DZD', 'ECS', 'ECV', 'EEK', 
+'EGP', 'ERN', 'ESA', 'ESB', 'ESP', 'ETB', 'EUR', 'FIM', 'FJD', 'FKP', 'FRF', 'GBP', 'GEK', 
+'GEL', ' GHC', 'GHP', 'GHS', 'GIP', 'GMD', 'GNE', 'GNF', 'GNS', 'GQE', 'GRD', 'GTQ', 'GWE', 
+'GWP', 'GYD', 'HKD', 'HNL', 'HRD', 'HRK', 'HTG', 'HUF', 'IDR', 'IEP', 'ILP', ' ILR', 'ILS', 
+'INR', 'IQD', 'IRR', 'ISJ', 'ISK', 'ITL', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 
+'KPW', 'KRW', 'KWD', 'KYD', 'KZT', 'LAJ', 'LAK', 'LBP', ' LKR', 'LRD', 'LSL', 'LSM', 'LTL', 
+'LTT', 'LUC', 'LUF', 'LUL', 'LVL', 'LVR', 'LYD', 'MAD', 'MDL', 'MGA', 'MGF', 'MKD', 'MLF', 
+'MMK', 'MNT', 'MOP', 'MRO', 'MRU', ' MTL', 'MTP', 'MUR', 'MVQ', 'MVR', 'MWK', 'MXN', 'MXP', 
+'MYR', 'MZE', 'MZM', 'MZN', 'NAD', 'NGN', 'NIC', 'NIO', 'NLG', 'NOK', 'NPR', 'NZD', 'OMR', 
+'PAB', 'PEH', ' PEI', 'PEN', 'PES', 'PGK', 'PHP', 'PKR', 'PLN', 'PLZ', 'PTE', 'PYG', 'QAR', 
+'RHD', 'ROK', 'ROL', 'RON', 'RSD', 'RUB', 'RUR', 'RWF', 'SAR', 'SBD', 'SCR', 'SDD', ' SDG', 
+'SDP', 'SEK', 'SGD', 'SHP', 'SIT', 'SKK', 'SLL', 'SOS', 'SRD', 'SRG', 'SSP', 'STD', 'STN', 
+'SUR', 'SVC', 'SYP', 'SZL', 'THB', 'TJR', 'TJS', 'TMM', 'TMT', ' TND', 'TOP', 'TPE', 'TRL', 
+'TRY', 'TTD', 'TWD', 'TZS', 'UAH', 'UAK', 'UGS', 'UGW', 'UGX', 'USD', 'USS', 'UYN', 'UYP', 
+'UYU', 'UZS', 'VEB', 'VEF', 'VNC', 'VND', ' VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XEU', 'XFO', 
+'XOF', 'XPF', 'YDD', 'YER', 'YUD', 'YUM', 'YUN', 'ZAL', 'ZAR', 'ZMK', 'ZMW', 'ZRN', 'ZRZ', 
+'ZWC', 'ZWD', 'ZWL', 'ZWN', 'ZWR', 'VUV', 'TND', 'SDG', 'LKR', 'BAM', 'BTN');
 
 -- Setup date of account initial balance
 -- https://github.com/moneymanagerex/moneymanagerex/issues/3554
